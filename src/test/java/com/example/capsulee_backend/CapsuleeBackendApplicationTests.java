ea1095b9863fbb948de9801ba339c5a7c132a324
only4wxx <orchidbyw1@gmail.com>
2025-11-09 03:14:19 +0900

    init: 스프링 초기 세팅

---
+++ src/test/java/com/example/capsulee_backend/CapsuleeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capsulee_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsuleeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
