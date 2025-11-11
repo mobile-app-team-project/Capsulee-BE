17c710fbd75e257a50254dde1b22f9cf68eb6cc7
only4wxx <orchidbyw1@gmail.com>
2025-11-11 14:32:04 +0900

    feat: loginID로 유저 아이디 찾기 함수

---
+++ src/main/java/com/example/capsulee_backend/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.capsulee_backend.user.repository;
+
+import com.example.capsulee_backend.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByLoginID(String loginID);
+}
