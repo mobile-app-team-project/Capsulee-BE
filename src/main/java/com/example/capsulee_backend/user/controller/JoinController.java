6c5ad69b38fa56d8466a70908bfd0d8fbd20d375
only4wxx <orchidbyw1@gmail.com>
2025-11-14 01:28:31 +0900

    chore: 회원가입 endpoint 수정

--- src/main/java/com/example/capsulee_backend/user/controller/JoinController.java
+++ src/main/java/com/example/capsulee_backend/user/controller/JoinController.java
@@ -5,14 +5,16 @@ import com.example.capsulee_backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class JoinController {
     private final UserService userService;
 
-    @PostMapping("/join")
+    @PostMapping("/register")
     public void join (@RequestBody JoinRequestDto joinRequestDto) {
         userService.join(joinRequestDto);
     }
