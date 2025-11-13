61469eedc135a380789321957923476e2330399b
only4wxx <orchidbyw1@gmail.com>
2025-11-13 20:37:10 +0900

    feat: password encoder 추가

---
+++ src/main/java/com/example/capsulee_backend/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.capsulee_backend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
