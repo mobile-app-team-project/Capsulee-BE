f1cef7c44aa284baee833d8e0aa1ed46c922bb90
only4wxx <orchidbyw1@gmail.com>
2025-11-13 20:42:59 +0900

    feat: 회원가입 컨트롤러 연결

---
+++ src/main/java/com/example/capsulee_backend/user/controller/JoinController.java
@@ -0,0 +1,19 @@
+package com.example.capsulee_backend.user.controller;
+
+import com.example.capsulee_backend.user.dto.request.JoinRequestDto;
+import com.example.capsulee_backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JoinController {
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public void join (@RequestBody JoinRequestDto joinRequestDto) {
+        userService.join(joinRequestDto);
+    }
+}
