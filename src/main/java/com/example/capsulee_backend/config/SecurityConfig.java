10d9ad04ee6156c318dfdba00a6342c11fdfad1a
only4wxx <orchidbyw1@gmail.com>
2025-11-14 01:55:56 +0900

    feat: SecurityFilterChain 추가하여 회원가입, 로그인에서는 인증 안된 사용자도 접근 가능하도록

--- src/main/java/com/example/capsulee_backend/config/SecurityConfig.java
+++ src/main/java/com/example/capsulee_backend/config/SecurityConfig.java
@@ -3,13 +3,27 @@ package com.example.capsulee_backend.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/**").authenticated()
+                );
+
+        return http.build();
+    }
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
