fcd2a4e3985af781e8f0f788d01a9f34c8edc860
only4wxx <orchidbyw1@gmail.com>
2025-11-14 09:34:45 +0900

    feat: DTO에 getter 추가

--- src/main/java/com/example/capsulee_backend/user/dto/response/LoginResponseDto.java
+++ src/main/java/com/example/capsulee_backend/user/dto/response/LoginResponseDto.java
@@ -1,7 +1,9 @@
 package com.example.capsulee_backend.user.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class LoginResponseDto {
     private String accessToken;
