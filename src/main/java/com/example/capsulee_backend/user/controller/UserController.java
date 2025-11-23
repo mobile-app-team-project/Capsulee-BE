25fe6415f3faf3a8ade2c572f76980512b44fbec
only4wxx <orchidbyw1@gmail.com>
2025-11-23 18:33:03 +0900

    feat: 내 프로필 수정 API 추가

--- src/main/java/com/example/capsulee_backend/user/controller/UserController.java
+++ src/main/java/com/example/capsulee_backend/user/controller/UserController.java
@@ -2,13 +2,13 @@ package com.example.capsulee_backend.user.controller;
 
 import com.example.capsulee_backend.config.jwt.PrincipalHandler;
 import com.example.capsulee_backend.user.domain.User;
+import com.example.capsulee_backend.user.dto.request.UserUpdateRequestDto;
 import com.example.capsulee_backend.user.dto.response.UserInfoResponseDto;
+import com.example.capsulee_backend.user.dto.response.UserUpdateResponseDto;
 import com.example.capsulee_backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +24,14 @@ public class UserController {
 
         UserInfoResponseDto userInfoResponseDto = userService.getUserInfo(user);
         return ResponseEntity.ok(userInfoResponseDto);
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<UserUpdateResponseDto> updateUserInfo(@RequestBody UserUpdateRequestDto userUpdateRequestDto) {
+        // 토큰에서 내 정보 가져오기
+        String userLoginID = PrincipalHandler.getLoginIDFromPrincipal();;
+        User user = userService.getUserByLoginID(userLoginID);
+
+        return ResponseEntity.ok(userService.updateUserInfo(user, userUpdateRequestDto));
     }
 }
