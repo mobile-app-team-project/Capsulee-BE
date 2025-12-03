028cafae19da0641e8d074fd1b5f621d9337b0b9
only4wxx <orchidbyw1@gmail.com>
2025-12-04 01:45:53 +0900

    feat: ready 상태 설정 API 연결

---
+++ src/main/java/com/example/capsulee_backend/capsule/controller/UnlockController.java
@@ -0,0 +1,40 @@
+package com.example.capsulee_backend.capsule.controller;
+
+import com.example.capsulee_backend.capsule.domain.Capsule;
+import com.example.capsulee_backend.capsule.dto.request.ReadyStatusRequestDto;
+import com.example.capsulee_backend.capsule.dto.response.ReadyStatusResponseDto;
+import com.example.capsulee_backend.capsule.service.CapsuleService;
+import com.example.capsulee_backend.capsule.service.UnlockService;
+import com.example.capsulee_backend.config.jwt.PrincipalHandler;
+import com.example.capsulee_backend.user.domain.User;
+import com.example.capsulee_backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/unlock")
+@RequiredArgsConstructor
+public class UnlockController {
+
+    private final UserService userService;
+    private final CapsuleService capsuleService;
+    private final UnlockService unlockService;
+
+    @PostMapping("/ready/{capsuleId}")
+    public ResponseEntity<ReadyStatusResponseDto> changeReadyStatus(
+            @PathVariable Long capsuleId,
+            @RequestBody ReadyStatusRequestDto readyStatusRequestDto
+    ) {
+        // 토큰에서 내 정보 가져오기
+        String userLoginID = PrincipalHandler.getLoginIDFromPrincipal();;
+        User user = userService.getUserByLoginID(userLoginID);
+
+        // 캡슐 정보 가져오기
+        Capsule capsule = capsuleService.getCapsuleById(capsuleId);
+
+        // ready 상태 업데이트
+        ReadyStatusResponseDto readyStatusResponseDto = unlockService.changeReadyStatus(capsule, user, readyStatusRequestDto);
+        return ResponseEntity.ok(readyStatusResponseDto);
+    }
+}
